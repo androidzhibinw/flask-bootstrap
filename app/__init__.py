2b5e07fc298db854569d8ae6c8953d4d025bc988
zhibinw <android_zhibinw@163.com>
2016-02-12 20:56:59 +0800

    update config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+import os
+
 
 DEBUG = True
 
+SECRET_KEY = 'secret key'
 
-SECRET_KEY =  'secret key'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+
+CSRF_ENABLED = True
+
+CSRF_SESSION_KEY = "secret"
