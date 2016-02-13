8526f33765406d0c880e7ebf42d1620ccbaf151a
zhibinw <android_zhibinw@163.com>
2016-02-13 21:08:33 +0800

    add post-all page

--- app/mod_post/controllers.py
+++ app/mod_post/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 
 mod_post = Blueprint('post', __name__, url_prefix='/post')
@@ -6,4 +6,4 @@ mod_post = Blueprint('post', __name__, url_prefix='/post')
 
 @mod_post.route('/all')
 def posts():
-    return 'posts'
+    return  render_template('post/all.html')
